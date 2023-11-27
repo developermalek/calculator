dee07cdaa2ab5aa912a197e15c65acd8c04c5371
Tohid Bin Azam <tohidbinazamsunny1@gmail.com>
2023-11-28 00:16:49 +0600

    change option value

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,10 @@ function App() {
   const [state, setState] = useState(0)
 
   const [input, inputChange, form] = vtexInput({
-    largura:0,
+    largura: 0,
     altura: 0,
-    comprimento:0,
-    densidade:""
+    comprimento: 0,
+    densidade: ""
   });
 
   const handleSubmit = (e) => {
@@ -66,17 +66,17 @@ function App() {
               <option>Abra este menu</option>
               <option value={0.000810}>D15 Branca</option>
               <option value={0.000930}>D20 Branca</option>
-              <option value={0.0001030}>D23 Branca</option>
-              <option value={0.0001170}>D27 Branca</option>
-              <option value={0.0001250}>D28 Azul</option>
-              <option value={0.0001280}>D28 Chumbo</option> 
-              <option value={0.0001350}>28 Premium Verde clara</option> 
-              <option value={0.0001280}>D33 Laranja</option>
-              <option value={0.0001630}>D33 Macia Laranja claro</option>
-              <option value={0.0001700}>D35 Amarela</option>
-              <option value={0.0002100}>D45 Chumbo</option>
-              <option value={0.0002200}>Aglomerado D80</option>
-              <option value={0.0001365}>D28 Macia Branca</option>
+              <option value={0.001030}>D23 Branca</option>
+              <option value={0.001170}>D27 Branca</option>
+              <option value={0.001250}>D28 Azul</option>
+              <option value={0.001280}>D28 Chumbo</option> 
+              <option value={0.001350}>28 Premium Verde clara</option> 
+              <option value={0.001280}>D33 Laranja</option>
+              <option value={0.001630}>D33 Macia Laranja claro</option>
+              <option value={0.001700}>D35 Amarela</option>
+              <option value={0.002100}>D45 Chumbo</option>
+              <option value={0.002200}>Aglomerado D80</option>
+              <option value={0.001365}>D28 Macia Branca</option>
             </Form.Select>
             <Form.Text className="text-muted">
               Densidade da espuma
